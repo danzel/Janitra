db6d94b7cb2a4104d4757c31b7d55e795fa62f76
danzel <danzel@localhost.geek.nz>
2017-09-10 21:14:35 +1200

    Test Definition uploading

--- Janitra.Api/Controllers/Api/TestDefinitionsController.cs
+++ Janitra.Api/Controllers/Api/TestDefinitionsController.cs
@@ -6,12 +6,8 @@ using AutoMapper;
 using Janitra.Api.Services;
 using Janitra.Data;
 using Janitra.Data.Models;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
 
 namespace Janitra.Api.Controllers.Api
 {
@@ -22,18 +18,14 @@ namespace Janitra.Api.Controllers.Api
 	public class TestDefinitionsController : Controller
 	{
 		private readonly JanitraContext _context;
-		private readonly CurrentUser _currentUser;
-		private readonly IFileStorageService _fileStorage;
 		private readonly IMapper _mapper;
 
 		/// <summary>
 		/// Constructor
 		/// </summary>
-		public TestDefinitionsController(JanitraContext context, CurrentUser currentUser, IFileStorageService fileStorage)
+		public TestDefinitionsController(JanitraContext context)
 		{
 			_context = context;
-			_currentUser = currentUser;
-			_fileStorage = fileStorage;
 
 			_mapper = CreateMapper();
 		}
@@ -69,54 +61,7 @@ namespace Janitra.Api.Controllers.Api
 				.Select(t => _mapper.Map<JsonTestDefinition>(t))
 				.ToArrayAsync();
 		}
-		/*
-		[Authorize(Roles = "Developer")]
-		[HttpPost("add")]
-		[ProducesResponseType(typeof(AddTestResult), StatusCodes.Status200OK)]
-		public async Task<IActionResult> Add([FromBody] NewTestDefinition newTest)
-		{
-			//newTestRom xor TestRomId
-			if (newTest.TestRomId.HasValue == (newTest.NewTestRom != null))
-				return BadRequest("Exactly one of TestRomId and NewTestRom must be set");
 
-			var test = _mapper.Map<TestDefinition>(newTest);
-			
-			//Add the test rom (or set based on the selected one)
-			if (newTest.NewTestRom != null)
-			{
-				var testRomUrl = await _fileStorage.StoreTestRom(newTest.NewTestRom.RomBytes);
-
-				var testRom = _mapper.Map<TestRom>(newTest.NewTestRom);
-				testRom.RomUrl = testRomUrl;
-				testRom.AddedByUser = _currentUser.User;
-				testRom.AddedAt = DateTimeOffset.UtcNow;
-				testRom.RomSha256 = SHA256Hash.HashBytes(newTest.NewTestRom.RomBytes);
-				await _context.AddAsync(testRom);
-				test.TestRom = testRom;
-			}
-			else
-			{
-				test.TestRomId = newTest.TestRomId.Value;
-			}
-
-			//Add the movie
-			test.MovieUrl = await _fileStorage.StoreMovie(newTest.MovieBytes);
-			test.MovieSha256 = SHA256Hash.HashBytes(newTest.MovieBytes);
-
-			//Add unmapped fields
-			test.ActivelyTesting = true;
-			test.AddedByUser = _currentUser.User;
-			test.AddedAt = DateTimeOffset.UtcNow;
-
-			await _context.AddAsync(test);
-			await _context.SaveChangesAsync();
-
-			return Ok(new AddTestResult
-			{
-				TestDefinitionId = test.TestDefinitionId
-			});
-		}
-		*/
 		public class JsonTestDefinition
 		{
 			[Required]
--- Janitra.Api/Controllers/TestDefinitionsController.cs
+++ Janitra.Api/Controllers/TestDefinitionsController.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
+using Janitra.Api.Services;
 using Janitra.Data;
+using Janitra.Data.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -13,15 +17,42 @@ namespace Janitra.Api.Controllers
 	public class TestDefinitionsController : Controller
 	{
 		private readonly JanitraContext _context;
+		private readonly CurrentUser _currentUser;
+		private readonly IFileStorageService _fileStorageService;
+		private readonly IMapper _mapper;
 
-		public TestDefinitionsController(JanitraContext context)
+		public TestDefinitionsController(JanitraContext context, IFileStorageService fileStorageService, CurrentUser currentUser)
 		{
 			_context = context;
+			_fileStorageService = fileStorageService;
+			_currentUser = currentUser;
+
+			_mapper = CreateMapper();
+		}
+
+		private IMapper CreateMapper()
+		{
+			var config = new MapperConfiguration(cfg =>
+			{
+				cfg.CreateMap<AddTestModel, TestDefinition>(MemberList.Source)
+					.ForSourceMember(atm => atm.MovieFile, o => o.Ignore())
+					.ForSourceMember(atm => atm.RomFile, o => o.Ignore())
+					.ForSourceMember(atm => atm.CodeUrl, o => o.Ignore());
+			});
+
+			config.AssertConfigurationIsValid();
+
+			return config.CreateMapper();
 		}
 
 		public async Task<IActionResult> Index()
 		{
 			return View(await _context.TestDefinitions.Include(td => td.TestRom).Where(b => b.ActivelyTesting).OrderBy(b => b.TestDefinitionId).ToArrayAsync());
+		}
+
+		public async Task<IActionResult> ViewTest(int id)
+		{
+			return View(await _context.TestDefinitions.SingleAsync(td => td.TestDefinitionId == id));
 		}
 
 		[HttpGet]
@@ -33,22 +64,53 @@ namespace Janitra.Api.Controllers
 
 		[HttpPost]
 		[Authorize(Roles = "Developer")]
-		//[ValidateAntiForgeryToken]
-		public IActionResult Add(AddTestModel test)
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Add(AddTestModel test)
 		{
 			if (ModelState.IsValid)
 			{
-				//TODO
-				Console.WriteLine();
+				var movieBytes = new byte[test.MovieFile.Length];
+				var movie = new MemoryStream(movieBytes);
+				await test.MovieFile.CopyToAsync(movie);
+
+				var romBytes = new byte[test.RomFile.Length];
+				var rom = new MemoryStream(romBytes);
+				await test.RomFile.CopyToAsync(rom);
+
+				var now = DateTimeOffset.UtcNow;
+
+				var def = new TestDefinition
+				{
+					ActivelyTesting = true,
+					AddedAt = now,
+					AddedByUser = _currentUser.User,
+					MovieUrl = await _fileStorageService.StoreTestRom(movieBytes),
+					MovieSha256 = SHA256Hash.HashBytes(movieBytes),
+					Notes = test.Notes,
+					TestName = test.TestName,
+					TestRom = new TestRom
+					{
+						AddedAt = now,
+						AddedByUser = _currentUser.User,
+						CodeUrl = test.CodeUrl,
+						RomSha256 = SHA256Hash.HashBytes(romBytes),
+						RomUrl = await _fileStorageService.StoreTestRom(romBytes)
+					}
+				};
+
+				await _context.AddAsync(def);
+				await _context.SaveChangesAsync();
+
+				return RedirectToAction("ViewTest", new { id = def.TestDefinitionId });
 			}
-			return View();
+			return View(test);
 		}
 	}
 
 	public class AddTestModel
 	{
 		[Required]
-		public string Name { get; set; }
+		public string TestName { get; set; }
 
 		[Required]
 		public string Notes { get; set; }
