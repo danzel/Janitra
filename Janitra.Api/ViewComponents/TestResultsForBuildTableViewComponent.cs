645a0e49a43ddf90dde974c3ebdf3685d9f7c388
danzel <danzel@localhost.geek.nz>
2017-09-14 20:30:22 +1200

    Table views for results and some other small UI tweaks

---
+++ Janitra.Api/ViewComponents/TestResultsForBuildTableViewComponent.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Janitra.Data;
+using Janitra.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Janitra.Api.ViewComponents
+{
+    public class TestResultsForBuildTableViewComponent : ViewComponent
+    {
+		private readonly JanitraContext _context;
+		public TestResultsForBuildTableViewComponent(JanitraContext context)
+		{
+			_context = context;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync(int citraBuildId)
+		{
+			//TODO: Limit this to the last X builds.
+
+			var results = await _context.TestResults
+				.Include(tr => tr.TestDefinition)
+				.Include(tr => tr.JanitraBot)
+				.Where(tr => tr.CitraBuildId == citraBuildId && tr.CitraBuild.BuildType == BuildType.CitraMaster && tr.TestDefinition.ActivelyTesting)
+				.ToArrayAsync();
+
+
+			var bots = results.Select(tr => tr.JanitraBot).Distinct().OrderBy(b => b.Name).ToArray();
+			var tests = results.Select(tr => tr.TestDefinition).Distinct().OrderBy(b => b.TestName).ToArray();
+
+			return View(new TestResultsForBuildTableViewModel(bots, tests, results));
+		}
+	}
+
+	public class TestResultsForBuildTableViewModel
+	{
+		public JanitraBot[] Bots { get; set; }
+		public TestDefinition[] Tests { get; set; }
+
+		public TestResult[] Results { get; set; } //TODO: This should have an efficient lookup on [bot/test]
+
+		public TestResultsForBuildTableViewModel(JanitraBot[] bots, TestDefinition[] tests, TestResult[] results)
+		{
+			Bots = bots;
+			Tests = tests;
+			Results = results;
+		}
+	}
+}
---
+++ Janitra.Api/ViewComponents/TestResultsForTestTableViewComponent.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Janitra.Data;
+using Janitra.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Janitra.Api.ViewComponents
+{
+    public class TestResultsForTestTableViewComponent : ViewComponent
+    {
+	    private readonly JanitraContext _context;
+	    public TestResultsForTestTableViewComponent(JanitraContext context)
+	    {
+		    _context = context;
+	    }
+
+		public async Task<IViewComponentResult> InvokeAsync(int testDefinitionId)
+		{
+			//TODO: Limit this to the last X builds.
+
+			var results = await _context.TestResults
+				.Include(tr => tr.CitraBuild)
+				.Include(tr => tr.JanitraBot)
+				.Where(tr => tr.TestDefinitionId == testDefinitionId && tr.CitraBuild.BuildType == BuildType.CitraMaster)
+				.ToArrayAsync();
+
+
+			var bots = results.Select(tr => tr.JanitraBot).Distinct().OrderBy(b => b.Name).ToArray();
+			var builds = results.Select(tr => tr.CitraBuild).Distinct().OrderByDescending(b => b.CitraBuildId).ToArray();
+
+			return View(new TestResultsForTestTableViewModel(bots, builds, results));
+		}
+	}
+
+	public class TestResultsForTestTableViewModel
+	{
+		public JanitraBot[] Bots { get; set; }
+		public CitraBuild[] Builds { get; set; }
+
+		public TestResult[] Results { get; set; } //TODO: This should have an efficient lookup on [bot/build]
+
+		public TestResultsForTestTableViewModel(JanitraBot[] bots, CitraBuild[] builds, TestResult[] results)
+		{
+			Bots = bots;
+			Builds = builds;
+			Results = results;
+		}
+	}
+}
