8b649b678d887f09f0bbecd51e4c440ee5b4839d
danzel <danzel@localhost.geek.nz>
2017-10-08 10:27:55 +1300

    Start on a Compare view, pretty basic

--- Janitra/Controllers/MovieResultsController.cs
+++ Janitra/Controllers/MovieResultsController.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
 using Janitra.Data;
 using Janitra.Data.Models;
@@ -21,14 +19,53 @@ namespace Janitra.Controllers
 
 		public async Task<IActionResult> View(int id)
 		{
-			return View(await _context.RomMovieResults
+			var romMovieResult = await _context.RomMovieResults
 				.Where(rmr => rmr.RomMovieResultId == id)
 				.Include(rmr => rmr.RomMovie).ThenInclude(rm => rm.Rom)
 				.Include(rmr => rmr.CitraBuild)
 				.Include(rmr => rmr.JanitraBot)
 				.Include(rmr => rmr.Screenshots)
-				.SingleAsync()
+				.SingleAsync();
+
+			var latestMaster = await _context.CitraBuilds.Where(cb => cb.BuildType == BuildType.CitraMaster).OrderByDescending(cb => cb.CommitTime).FirstAsync();
+
+			var masterMovieFromSameBot = await _context.RomMovieResults.SingleOrDefaultAsync(rmr => rmr.JanitraBotId == romMovieResult.JanitraBotId && rmr.CitraBuildId == latestMaster.CitraBuildId && rmr.RomMovieId == romMovieResult.RomMovieId);
+
+			return View(new MovieResultViewViewModel
+				{
+					Result = romMovieResult,
+					MasterResultId = masterMovieFromSameBot?.RomMovieResultId
+				}
 			);
 		}
+
+		public async Task<IActionResult> Compare(int leftId, int rightId)
+		{
+			return View(new MovieResultsCompareViewModel
+			{
+				Left = await _context.RomMovieResults
+					.Include(rmr => rmr.CitraBuild)
+					.Include(rmr => rmr.RomMovie).ThenInclude(rm => rm.Rom)
+					.Include(rmr => rmr.Screenshots)
+					.SingleAsync(rmr => rmr.RomMovieResultId == leftId),
+				Right = await _context.RomMovieResults
+					.Include(rmr => rmr.CitraBuild)
+					.Include(rmr => rmr.Screenshots)
+					.SingleAsync(rmr => rmr.RomMovieResultId == rightId)
+			});
+		}
+	}
+
+	public class MovieResultViewViewModel
+	{
+		public RomMovieResult Result { get; set; }
+
+		public int? MasterResultId { get; set; }
+	}
+
+	public class MovieResultsCompareViewModel
+	{
+		public RomMovieResult Left { get; set; }
+		public RomMovieResult Right { get; set; }
 	}
 }
